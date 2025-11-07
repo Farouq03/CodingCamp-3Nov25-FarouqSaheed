8b19c9e825a4af45ac0f88e5de0f3e2c82aa0cb2
Farouq03 <farouqsaheed@gmail.com>
2025-11-08 00:47:44 +0700

    Update ulang Fixed

---
+++ js/script.js
@@ -0,0 +1,23 @@
+welcomeMessage();
+function welcomeMessage() {
+    let name = prompt("Please enter your name:");
+    if (name != "null" && name != "") {
+        document.getElementById("welcome-message").innerHTML = "Hi " + name + ", Welcome to the Website!";
+    }
+}
+
+const Nama = document.getElementById("Nama");
+const Tanggal_lahir = document.getElementById("Tanggal_lahir");
+const Jenis_Kelamin = document.querySelector('input[name="Jenis_Kelamin"]:checked');
+const Pesan = document.getElementById("Pesan");
+
+function submitForm() {
+    if (!Nama.value || !Tanggal_lahir.value || !document.querySelector('input[name="Jenis_Kelamin"]:checked') || !Pesan.value) {
+        alert("Please fill in all fields before submitting the form.");
+    } else {
+        document.getElementById("submitted-form").innerHTML = Nama.value + "<br>" +
+            Tanggal_lahir.value + "<br>" +
+            document.querySelector('input[name="Jenis_Kelamin"]:checked').value + "<br>" +
+            Pesan.value;
+    }
+}
